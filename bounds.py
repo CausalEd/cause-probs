8fa066c870d4f02fc3a5a024961c427aa6d20057
Scott Mueller <scott@cs.ucla.edu>
2023-01-18 23:45:49 -0800

    added danger notice for incompatible data

--- bounds.py
+++ bounds.py
@@ -6,16 +6,22 @@ def less_equal(x, y):
 def greater(x, y):
     return x > y and not math.isclose(x, y)
 
+st.set_page_config(layout = 'wide')
+
 st.header("Bounds on Probabilities of Causation")
-# pyx = P(yₓ), pyxp = P(yₓ')
-st.subheader('Experimental Probabilities')
-pyx = st.slider('$P(y_x)$', 0., 1., 0.5, step=0.01)
-pyxp = st.slider('$P(y_{x\'})$', 0., 1., 0.5, step=0.01)
-# px = P(x), pycx = P(y|x), pycxp = P(y|x')
-st.subheader('Observational Probabilities')
-px = st.slider('$P(x)$', 0., 1., 0.5, step=0.01)
-pycx = st.slider('$P(y|x)$', 0., 1., 0.5, step=0.01)
-pycxp = st.slider('$P(y|{x\'})$', 0., 1., 0.5, step=0.01)
+col1, col2 = st.columns(2)
+
+with col1:
+    # pyx = P(yₓ), pyxp = P(yₓ')
+    st.subheader('Experimental Probabilities')
+    pyx = st.slider('$P(y_x)$', 0., 1., 0.5, step=0.01)
+    pyxp = st.slider('$P(y_{x\'})$', 0., 1., 0.5, step=0.01)
+with col2:
+    # px = P(x), pycx = P(y|x), pycxp = P(y|x')
+    st.subheader('Observational Probabilities')
+    px = st.slider('$P(x)$', 0., 1., 0.5, step=0.01)
+    pycx = st.slider('$P(y|x)$', 0., 1., 0.5, step=0.01)
+    pycxp = st.slider('$P(y|{x\'})$', 0., 1., 0.5, step=0.01)
 
 ate = pyx - pyxp
 
@@ -29,6 +35,10 @@ pxyp = pypcx * px
 pxpy = pycxp * pxp
 pxpyp = pypcxp * pxp
 py = pxy + pxpy
+
+incompatible = greater(pxy, pyx) or greater(pyx, pxy + pxp) or greater(pxpy, pyxp) or greater(pyxp, pxpy + px)
+if incompatible:
+    st.header(':fire: Danger: Experimental & Observational Probabilities Incompatible! :fire:')
 
 # lb and ub are lower and upper bounds of P(benefit)
 lb = max(0, pyx - pyxp, py - pyxp, pyx - py)
@@ -45,5 +55,5 @@ st.markdown(f'${lb_doomed:.2f} \\leqslant P(\\text{{doomed}}) \\leqslant {ub_doo
 f'${lb:.2f} \\leqslant P(\\text{{benefit}}) \\leqslant {ub:.2f}$  \n'
 f'${lb_doomed:.2f} \\leqslant P(\\text{{harm}}) \\leqslant {ub_doomed:.2f}$  \n'
 f'${lb_immune:.2f} \\leqslant P(\\text{{immune}}) \\leqslant {ub_immune:.2f}$')
-if greater(pxy, pyx) or greater(pyx, pxy + pxp) or greater(pxpy, pyxp) or greater(pyxp, pxpy + px):
-    st.warning('Experimental and observational data incompatible')
+if incompatible:
+    st.warning('Experimental and observational data incompatible', icon = '🚨')
